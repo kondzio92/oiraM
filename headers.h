c66d757c5b151131bf69559e33c9a3ad51419d6f
Jakub Chy?kowski <chylek@mat.umk.pl>
2016-06-05 13:30:13 +0200

    code indents refactoring using vim
    gg
    Ctrl + V
    Ctrl + G
    =

--- headers.h
+++ headers.h
@@ -44,158 +44,158 @@ enum Direction{
 };
 
 class Object{
-	public:
-		Object(unsigned char type){
-			sprite.setOrigin(sprite.getLocalBounds().width/2,0);
-			this->type = type;
-			create = true;
-			clock.restart();
-		}
+    public:
+        Object(unsigned char type){
+            sprite.setOrigin(sprite.getLocalBounds().width/2,0);
+            this->type = type;
+            create = true;
+            clock.restart();
+        }
 
-		void setPosition(int x, int y){
-			sprite.setPosition(x, y);
-			position = sf::Vector2f(x, y);
-			clock.restart();
-			start_pos.x = position.x;
-		}
+        void setPosition(int x, int y){
+            sprite.setPosition(x, y);
+            position = sf::Vector2f(x, y);
+            clock.restart();
+            start_pos.x = position.x;
+        }
 
-		void startMoving(Direction dir){
-			clock.restart();
-			start_pos.x = position.x;
-			
-			if (dir == Direction::Left){
-				move |= 1;
-				move &= ~2;
-				sprite.setScale(1,1);
+        void startMoving(Direction dir){
+            clock.restart();
+            start_pos.x = position.x;
+
+            if (dir == Direction::Left){
+                move |= 1;
+                move &= ~2;
+                sprite.setScale(1,1);
             }else if(dir == Direction::Right){
-            	move |= 2;
-				move &= ~1;
-				sprite.setScale(-1,1);
+                move |= 2;
+                move &= ~1;
+                sprite.setScale(-1,1);
             }
-		}
+        }
 
-		void stopMoving(){
-			if((move & 3) !=0){
-				move &= ~3;
-				start_pos.x = position.x;
-			}
-		}
+        void stopMoving(){
+            if((move & 3) !=0){
+                move &= ~3;
+                start_pos.x = position.x;
+            }
+        }
 
-		void startFalling(){
-			falling_clock.restart();
-			start_pos.y = position.y;
-			move |= 8;
-		}
+        void startFalling(){
+            falling_clock.restart();
+            start_pos.y = position.y;
+            move |= 8;
+        }
 
-		void resetFalling(){
-			falling_clock.restart();
-			start_pos.y = position.y;
-		}
+        void resetFalling(){
+            falling_clock.restart();
+            start_pos.y = position.y;
+        }
 
-		void stopFalling(){
-			move &= ~8;
-			start_pos.y = position.y;
-		}
+        void stopFalling(){
+            move &= ~8;
+            start_pos.y = position.y;
+        }
 
-		void jump(){
-			if((move & 4) == 0){
-				jump_speed = MAX(speed, MIN_JUMP_SPEED) * 2.0f;
-				if((move & 3) !=0)
-					jump_speed = MAX(speed + MIN(ACCEL * clock.getElapsedTime().asSeconds(), MAX_SPEED - speed), MIN_JUMP_SPEED) * 2.0f;
-	            start_pos.y = position.y;
-	            jump_clock.restart();
-	            move |= 4;
-			}
-		}
+        void jump(){
+            if((move & 4) == 0){
+                jump_speed = MAX(speed, MIN_JUMP_SPEED) * 2.0f;
+                if((move & 3) !=0)
+                    jump_speed = MAX(speed + MIN(ACCEL * clock.getElapsedTime().asSeconds(), MAX_SPEED - speed), MIN_JUMP_SPEED) * 2.0f;
+                start_pos.y = position.y;
+                jump_clock.restart();
+                move |= 4;
+            }
+        }
 
-		void execute(){
-			last_pos = position;
-			way_diff = sf::Vector2f(0,0);
-			if((move & 1) != 0){
-				sec = clock.getElapsedTime().asSeconds();
-				way_diff.x = -(speed + ACCEL * MIN(sec, MAX_SPEED/ACCEL)) * sec; /* s = vt + (at^2)/2  ->  s = (t + at/2) * t,  where ACCEL = a/2 v */	
-			}else if((move & 2) != 0){
-				sec = clock.getElapsedTime().asSeconds();
-				way_diff.x = (speed + ACCEL * MIN(sec, MAX_SPEED/ACCEL)) * sec; /* s = vt + (at^2)/2  ->  s = (t + at/2) * t,  where ACCEL = a/2 v */
-			}
+        void execute(){
+            last_pos = position;
+            way_diff = sf::Vector2f(0,0);
+            if((move & 1) != 0){
+                sec = clock.getElapsedTime().asSeconds();
+                way_diff.x = -(speed + ACCEL * MIN(sec, MAX_SPEED/ACCEL)) * sec; /* s = vt + (at^2)/2  ->  s = (t + at/2) * t,  where ACCEL = a/2 v */
+            }else if((move & 2) != 0){
+                sec = clock.getElapsedTime().asSeconds();
+                way_diff.x = (speed + ACCEL * MIN(sec, MAX_SPEED/ACCEL)) * sec; /* s = vt + (at^2)/2  ->  s = (t + at/2) * t,  where ACCEL = a/2 v */
+            }
 
-           if((move & 4) != 0){
-            	sec = jump_clock.getElapsedTime().asSeconds();
-				way_diff.y -= jump_speed * sec;
-			}
+            if((move & 4) != 0){
+                sec = jump_clock.getElapsedTime().asSeconds();
+                way_diff.y -= jump_speed * sec;
+            }
 
-			if((move & 8) != 0){
-				sec = falling_clock.getElapsedTime().asSeconds();
-				way_diff.y += G_ACCEL * sec * sec;
-			}
-			position = start_pos + way_diff;
-			
+            if((move & 8) != 0){
+                sec = falling_clock.getElapsedTime().asSeconds();
+                way_diff.y += G_ACCEL * sec * sec;
+            }
+            position = start_pos + way_diff;
+
             if(position.x < 0.0f)
                 position.x = 0.0f;
             if(position.x > 800)
                 position.x = 800;
-		}
+        }
 
-		bool colision(Object *o){
-			float w = sprite.getGlobalBounds().width;
-			float h = sprite.getGlobalBounds().height;
-			float w1 = o->sprite.getGlobalBounds().width;
-			float h1 = o->sprite.getGlobalBounds().height;
+        bool colision(Object *o){
+            float w = sprite.getGlobalBounds().width;
+            float h = sprite.getGlobalBounds().height;
+            float w1 = o->sprite.getGlobalBounds().width;
+            float h1 = o->sprite.getGlobalBounds().height;
 
-			if(fabs(o->position.x-position.x)<=w1/2+w/2 &&
-				fabs(o->position.y-position.y)<=h1/2+h/2){
-				float m1 = MIN(position.x+w/2, o->position.x+w1/2)-MAX(position.x-w/2, o->position.x-w1/2);
-				float m2 = MIN(position.y+h/2, o->position.y+h1/2) -MAX(position.y-h/2, o->position.y-h1/2);
-				if(m1<1 && m2<1)
-					return false;
+            if(fabs(o->position.x-position.x)<=w1/2+w/2 &&
+                    fabs(o->position.y-position.y)<=h1/2+h/2){
+                float m1 = MIN(position.x+w/2, o->position.x+w1/2)-MAX(position.x-w/2, o->position.x-w1/2);
+                float m2 = MIN(position.y+h/2, o->position.y+h1/2) -MAX(position.y-h/2, o->position.y-h1/2);
+                if(m1<1 && m2<1)
+                    return false;
 
-				if(m1 > m2){
-					position.y = o->position.y + (position.y>o->position.y?1:-1) * (h1/2 + h/2);
-				}else{
-					position.x = o->position.x + (position.x>o->position.x?1:-1) * (w1/2 + w/2 +1);
-					start_pos.x = position.x;
-					clock.restart();					
-				}
-				
-				move &= ~4;
-				resetFalling();
-				return true;
-			}
-				
-			return false;
+                if(m1 > m2){
+                    position.y = o->position.y + (position.y>o->position.y?1:-1) * (h1/2 + h/2);
+                }else{
+                    position.x = o->position.x + (position.x>o->position.x?1:-1) * (w1/2 + w/2 +1);
+                    start_pos.x = position.x;
+                    clock.restart();
+                }
 
-		}
+                move &= ~4;
+                resetFalling();
+                return true;
+            }
 
-		void backToLastCorrectPosition(){
-			move &= ~3;
-			sprite.setPosition(last_pos);
-		}
+            return false;
 
-		void setTexture(sf::Texture &t){
-			sprite.setTexture(t);
-			sprite.setOrigin(t.getSize().x/2, t.getSize().y/2);
-		}
+        }
 
-		void update(){
-			sprite.setPosition(position);
-		}
+        void backToLastCorrectPosition(){
+            move &= ~3;
+            sprite.setPosition(last_pos);
+        }
 
-		sf::FloatRect getSize(){
-			return sprite.getGlobalBounds();
-		}
+        void setTexture(sf::Texture &t){
+            sprite.setTexture(t);
+            sprite.setOrigin(t.getSize().x/2, t.getSize().y/2);
+        }
 
-		float sec, speed = MIN_SPEED, jump_speed = -1.0f;
-		sf::Vector2f position, way_diff=sf::Vector2f(0,0), start_pos, last_pos, falling_start_pos;
-		sf::Clock clock, jump_clock, falling_clock;
-		char move = 0;
-		bool create;
-		unsigned char type;
-		sf::Sprite sprite;
+        void update(){
+            sprite.setPosition(position);
+        }
+
+        sf::FloatRect getSize(){
+            return sprite.getGlobalBounds();
+        }
+
+        float sec, speed = MIN_SPEED, jump_speed = -1.0f;
+        sf::Vector2f position, way_diff=sf::Vector2f(0,0), start_pos, last_pos, falling_start_pos;
+        sf::Clock clock, jump_clock, falling_clock;
+        char move = 0;
+        bool create;
+        unsigned char type;
+        sf::Sprite sprite;
 };
 
 struct Shared{
-	std::vector<Object*> objects, bg_objects, enemys;
-	unsigned char game_state;
+    std::vector<Object*> objects, bg_objects, enemys;
+    unsigned char game_state;
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -14,104 +14,104 @@
 void loadWorld(std::string filename, Object *player, std::vector<Object*> &objects, std::vector<Object*> &bg, std::vector<Object*> &animated);
 
 int main (int argc, char **argv) {
-	#ifdef __linux__
+#ifdef __linux__
     XInitThreads();
-    #endif
-	Shared shared;
-	Object player(ObjectType::Player);
-	player.startFalling();
-	shared.objects.push_back(&player);
-	loadWorld("map",&player, shared.objects, shared.bg_objects, shared.enemys);
-	char move=0;
-	shared.game_state = 1; // start screen - main menu or game
+#endif
+    Shared shared;
+    Object player(ObjectType::Player);
+    player.startFalling();
+    shared.objects.push_back(&player);
+    loadWorld("map",&player, shared.objects, shared.bg_objects, shared.enemys);
+    char move=0;
+    shared.game_state = 1; // start screen - main menu or game
 
-	ThreadWindow window(WIDTH, HEIGHT, "oiraM", &shared);
+    ThreadWindow window(WIDTH, HEIGHT, "oiraM", &shared);
 
-	while(!window.isOpen());
-	while(window.isOpen()){
-		if(shared.game_state == 1){		//game
-			if (((move & 1) == 0) && sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
+    while(!window.isOpen());
+    while(window.isOpen()){
+        if(shared.game_state == 1){		//game
+            if (((move & 1) == 0) && sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
                 move |= 1;
                 player.startMoving(Left);
-			}else if(!sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
-				move &= ~1;
-			}
+            }else if(!sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
+                move &= ~1;
+            }
 
-			if (((move & 2) == 0) && sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){   
-                    move |= 2;
-                    player.startMoving(Right);
-			}else if(!sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
-				move &= ~2;
-			}
+            if (((move & 2) == 0) && sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+                move |= 2;
+                player.startMoving(Right);
+            }else if(!sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+                move &= ~2;
+            }
 
-			if((move & 3) ==0){
-				player.stopMoving();
-			}
+            if((move & 3) ==0){
+                player.stopMoving();
+            }
 
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)){
-				move |= 4;
-				player.jump();
-			}
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)){
+                move |= 4;
+                player.jump();
+            }
 
-			player.execute();
+            player.execute();
 
-			for(Object *o : shared.objects){
-				if(o!=&player){
-					player.colision(o);
-				}
-			}
-			player.update();
+            for(Object *o : shared.objects){
+                if(o!=&player){
+                    player.colision(o);
+                }
+            }
+            player.update();
 
-		}else if(shared.game_state == 2){	// main menu screen
-			if (sf::Mouse::isButtonPressed(sf::Mouse::Left)){
-				sf::Vector2i clickPosition = sf::Mouse::getPosition(*window.getWindow());
-				if(clickPosition.x > 10 && clickPosition.x < 100 && 	//start button
-				   clickPosition.y > 10 && clickPosition.y < 100){
-					shared.game_state = 1;
-				}
-			}
-		}
-	}
+        }else if(shared.game_state == 2){	// main menu screen
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+                sf::Vector2i clickPosition = sf::Mouse::getPosition(*window.getWindow());
+                if(clickPosition.x > 10 && clickPosition.x < 100 && 	//start button
+                        clickPosition.y > 10 && clickPosition.y < 100){
+                    shared.game_state = 1;
+                }
+            }
+        }
+    }
 
     return 0;
 }
 
 void loadWorld(std::string filename, Object *player, std::vector<Object*> &objects, std::vector<Object*> &bg, std::vector<Object*> &animated){
-	std::ifstream file(filename);
+    std::ifstream file(filename);
 
-	if(file.is_open()){
-		std::string str;
-		int x=780, y=0;
-		while(std::getline(file, str)){
-			y=20;
-			std::cout<<str<<std::endl;
-			for(char  &c: str){
-				std::cout<<x<<"\t"<<y<<std::endl;
-				switch(c){
-					case 'X':
-						objects.push_back(new Object(ObjectType::Rock));
-						objects.back()->setPosition(x, y);
-						break;
-					case 'P':
-						player->setPosition(x, y);
-						break;
-					case 'C':
-						bg.push_back(new Object(ObjectType::Sky));
-						bg.back()->setPosition(x, y);
-						break;
-					case 'G':
-						objects.push_back(new Object(ObjectType::Grass));
-						objects.back()->setPosition(x, y);
-						break;
-				}
-				y+=40;
-			}
-			x-=40;
-			std::cout<<std::endl;
-		}
-	}else{
-		fprintf(stderr, "ERROR: Cannot load file world");
+    if(file.is_open()){
+        std::string str;
+        int x=780, y=0;
+        while(std::getline(file, str)){
+            y=20;
+            std::cout<<str<<std::endl;
+            for(char  &c: str){
+                std::cout<<x<<"\t"<<y<<std::endl;
+                switch(c){
+                    case 'X':
+                        objects.push_back(new Object(ObjectType::Rock));
+                        objects.back()->setPosition(x, y);
+                        break;
+                    case 'P':
+                        player->setPosition(x, y);
+                        break;
+                    case 'C':
+                        bg.push_back(new Object(ObjectType::Sky));
+                        bg.back()->setPosition(x, y);
+                        break;
+                    case 'G':
+                        objects.push_back(new Object(ObjectType::Grass));
+                        objects.back()->setPosition(x, y);
+                        break;
+                }
+                y+=40;
+            }
+            x-=40;
+            std::cout<<std::endl;
+        }
+    }else{
+        fprintf(stderr, "ERROR: Cannot load file world");
         exit(-1);
-	}
-	file.close();
+    }
+    file.close();
 }
